8a46710b5a80920872e39665579ebb63ff1793e5
vlada <vlada-ternova@example.com>
2023-07-10 11:57:11 +0300

    task1

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите A:");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите B:");
+int b = int.Parse(ReadLine()!);
+
+WriteLine(PrintNumbers(a, b));
+string PrintNumbers(int a, int b)
+{
+    if (b == a || b == 1)
+    {
+        return 1;
+    }
+    int s = PrintNumbers(a,b - 1) + b;
+    return s;
+}
---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите A:");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите B:");
+int b = int.Parse(ReadLine()!);
+
+WriteLine(AsyncCallback(a,b));
+int ack(int n, int m)
+{
+    while (n !=0)
+    {
+        if(m==0) m=1;
+        else m = ack(n, m-1);
+        n = n-1;
+    }
+    return m + 1;
+}
+//В задаче m и n неверно стоят, поэтому 29, вместо 9
